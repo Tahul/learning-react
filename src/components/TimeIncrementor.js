241ed296c8133cbc806608f71fe301d6aa45e360
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-09-02 23:10:43 +0200

    :sparkles: (time-incrementor) added pause function on TimeIncrementor comp

--- src/components/TimeIncrementor.js
+++ src/components/TimeIncrementor.js
@@ -8,28 +8,55 @@ export default class Incrementor extends React.Component {
     constructor(props) {
         super(props);
 
-        this.timer = null
-
         this.state = {
-            n: this.props.start
-        }
+            n: this.props.start,
+            timer: null
+        };
     }
 
     componentDidMount() {
-        this.timer = window.setInterval(this.tick.bind(this), 1000)
+        this.startTimer();
     }
 
     componentWillUnmount() {
-        window.clearInterval(this.timer)
+        this.stopTimer();
+    }
+
+    startTimer() {
+        this.setState({
+            timer: window.setInterval(this.tick.bind(this), 1000)
+        });
+    }
+
+    stopTimer() {
+        window.clearInterval(this.state.timer);
+
+        this.setState({
+            timer: null
+        });
     }
 
     tick() {
-        this.setState((state, props) => ({n: state.n + props.step}))
+        this.setState((state, props) => ({n: state.n + props.step}));
+    }
+
+    toggle() {
+        if (this.state.timer) {
+            this.stopTimer();
+        } else {
+            this.startTimer();
+        }
     }
 
     render() {
         return <div>
             Le nombre actuel est: { this.state.n }
+
+            <p>
+                <button onClick={this.toggle.bind(this)}>
+                    { this.state.timer ? "Pause" : "Play" }
+                </button>
+            </p>
         </div>
     }
 }
